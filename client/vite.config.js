d8295223b84c74c9d4c323946b1447cca69fb61d
Rodrigo Fúnes <rodrigofunes@hotmail.es>
2026-02-24 18:26:09 -0600

    hotfix: add import

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
-
+import packageJson from './package.json';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
